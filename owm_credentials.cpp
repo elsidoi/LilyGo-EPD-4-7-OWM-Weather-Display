3eccba122ff454b17f9c760d6b42ed35046a0644
Alexey <a.sizyukhin@yandex.ru>
2022-06-14 10:25:38 +0300

    Oops! Credentials leaked!

--- owm_credentials.cpp
+++ owm_credentials.cpp
@@ -7,7 +7,7 @@ const char* ssid     = "masha2";
 const char* password = "justintimberlake";
 
 // Use your own API key by signing up for a free developer account at https://openweathermap.org/
-String apikey       = "ffeccb53c3ce9f9264c468e29dce6c39";                      // See: https://openweathermap.org/
+String apikey       = "your openweathermap token here";                      // See: https://openweathermap.org/
 const char server[] = "api.openweathermap.org";
 //http://api.openweathermap.org/data/2.5/forecast?q=Melksham,UK&APPID=your_OWM_API_key&mode=json&units=metric&cnt=40
 //http://api.openweathermap.org/data/2.5/weather?q=Melksham,UK&APPID=your_OWM_API_key&mode=json&units=metric&cnt=1
